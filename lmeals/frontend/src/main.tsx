08b8e65147eb0c7ef773b853544fc7a5cd6ddfda
iddo lent <iddolent@gmail.com>
2026-01-27 18:58:41 +0200

    fix: resolve syntax error in main.tsx preventing docker build

--- lmeals/frontend/src/main.tsx
+++ lmeals/frontend/src/main.tsx
@@ -9,10 +9,7 @@ initRippleEffect();
 
 
 createRoot(document.getElementById('root')!).render(
-
-
-  createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  )
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
