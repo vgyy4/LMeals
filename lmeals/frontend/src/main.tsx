d5705dc5340d1dae5b15c2eb17a1b95026c783c8
iddo lent <iddolent@gmail.com>
2026-02-03 17:56:18 +0200

    fix: type cast crypto.randomUUID polyfill

--- lmeals/frontend/src/main.tsx
+++ lmeals/frontend/src/main.tsx
@@ -9,7 +9,8 @@ initRippleEffect();
 
 // Polyfill for crypto.randomUUID in non-secure contexts (HTTP)
 if (!crypto.randomUUID) {
-  crypto.randomUUID = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (crypto as any).randomUUID = () => {
     return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
       (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
     );
